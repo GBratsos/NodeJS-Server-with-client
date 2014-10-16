1184d1d162f56cccfab011ea3aa89eb18dc87e53
George Bratsos <geobratsos@gmail.com>
2014-10-16 15:38:29 +0300

    Progress

--- client/js/controllers/home.js
+++ client/js/controllers/home.js
@@ -2,12 +2,39 @@
 
 var myAppControllers = angular.module('myAppControllers', ['ngRoute'])
 
-myAppControllers.controller('myAppHomeCtrl', ['$scope','homeServices', function($scope, homeServices) {
-	var bratsos = 'bratsaki1213'; // Den emfanizetai pouthena, local metavliti
-	$scope.bratsaki = 'Eimai to Bratsaki'; // tha vgei sto HTML
-	homeServices.getVillage().then(function(data) {
+myAppControllers.controller('myAppPortfoliosCtrl', ['$scope','mainMenuServices', 'subMenuServices', 'topSlideshowServices', 'portfolioServices', function($scope, mainMenuServices, subMenuServices, topSlideshowServices, portfolioServices) {
+	mainMenuServices.getMainmenu().then(function(data) {
 		console.log(data);
-		$scope.antonis = data;
+		$scope.name_en = data;
+	});
+
+	subMenuServices.getSubmenu().then(function(data) {
+		console.log(data);	
+		$scope.sname_en = data;
+	});
+
+	topSlideshowServices.getTopSlideshow().then(function(data) {
+		console.log(data);	
+		$scope.title_en = data;
+		$scope.message_en = data;
+		$scope.details1_en = data;
+		$scope.details2_en = data;
+		$scope.details3_en = data;
+		$scope.details4_en = data;
+		$scope.img = data;
+	});
+	
+	portfolioServices.getPortfolio().then(function(data) {
+		console.log(data);
+		$scope.img = data;
 	});
 
 }]);
+
+//myAppControllers.controller('myAppPortfolioCtrl', ['$scope','portfolioServices', function($scope, portfolioServices) {
+//	portfolioServices.getportfolio().then(function(data) {
+//		console.log(data);
+//		$scope.img = data;
+//	});
+
+//}]);
